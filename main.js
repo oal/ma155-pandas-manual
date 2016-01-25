61877705f851f63d5b9b9638c07bbee44f28a0b2
Olav Lindekleiv <olav@lindekleiv.com>
2016-01-25 10:21:19 +0100

    Bruk av Jupyter.

--- main.js
+++
@@ -1,7 +0,0 @@
-
-$(function() {	
-	$('.menu a').on('click', function() {
-		console.log($(this).text())
-		$('.content').load(encodeURI('html/'+$(this).text()+'.ipynb'), function(){console.log(123)});
-	})
-})
